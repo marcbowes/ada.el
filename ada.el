339258591342111181e1b036f7b1e4411b886f9e
Marc Bowes <15209+marcbowes@users.noreply.github.com>
2025-03-19 10:16:01 -0700

    better names

--- ada.el
+++ ada.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc
 ;; Homepage: https://github.com/marcbowes/ada
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "26.2"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -19,7 +19,30 @@
 ;;
 ;;; Code:
 
-(defun fetch-ada-credentials (profile)
+(defvar ada--credentials-cache nil
+  "Cache for storing AWS credentials. Format: (profile credentials expiration-time)")
+
+(defun ada-get-credentials (profile)
+  "Get AWS credentials for given profile, using cache if valid"
+  (let* ((cached (assoc profile ada--credentials-cache))
+         (now (current-time))
+         (cached-expiration (and cached (ada--parse-iso8601-time-string (nth 3 (cadr cached)))))
+         (cached-credentials (and cached (cadr cached))))
+    (if (and cached-credentials
+             cached-expiration
+             (time-less-p now cached-expiration))
+        ;; Return cached credentials without expiration time
+        (butlast cached-credentials)
+      ;; Fetch new credentials
+      (let ((new-credentials (ada--fetch-credentials profile)))
+        ;; Update cache
+        (setq ada--credentials-cache
+              (cons (list profile new-credentials)
+                    (assoc-delete-all profile ada--credentials-cache)))
+        ;; Return credentials without expiration time
+        (butlast new-credentials)))))
+
+(defun ada--fetch-credentials (profile)
   "Fetch AWS credentials from ada CLI for given profile and return as list of strings"
   (let* ((json-string (shell-command-to-string (format "ada credentials print --profile %s" profile)))
          (json-data (json-read-from-string json-string)))
@@ -28,30 +51,7 @@
           (cdr (assoc 'SessionToken json-data))
           (cdr (assoc 'Expiration json-data)))))
 
-(defvar ada-credentials-cache nil
-  "Cache for storing AWS credentials. Format: (profile credentials expiration-time)")
-
-(defun get-ada-credentials (profile)
-  "Get AWS credentials for given profile, using cache if valid"
-  (let* ((cached (assoc profile ada-credentials-cache))
-         (now (current-time))
-         (cached-expiration (and cached (parse-iso8601-time-string (nth 3 (cadr cached)))))
-         (cached-credentials (and cached (cadr cached))))
-    (if (and cached-credentials
-             cached-expiration
-             (time-less-p now cached-expiration))
-        ;; Return cached credentials without expiration time
-        (butlast cached-credentials)
-      ;; Fetch new credentials
-      (let ((new-credentials (fetch-ada-credentials profile)))
-        ;; Update cache
-        (setq ada-credentials-cache
-              (cons (list profile new-credentials)
-                    (assoc-delete-all profile ada-credentials-cache)))
-        ;; Return credentials without expiration time
-        (butlast new-credentials)))))
-
-(defun parse-iso8601-time-string (time-string)
+(defun ada--parse-iso8601-time-string (time-string)
   "Convert ISO8601 time string to Emacs time value"
   (encode-time (parse-time-string time-string)))
 
